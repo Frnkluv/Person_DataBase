2f84f541a4179c264ea5ffdc4250d190571f9b90
Egor <Egor@ASUS-EGORM>
2022-04-25 20:17:07 +0300

    Добавьте файлы проекта.

---
+++ Person_DataBase/Program.cs
@@ -0,0 +1,38 @@
+﻿Person person = GetPerson();
+
+string phoneNumber = person?.Contacts?.PhoneNumber ?? "нет данных";
+
+Console.WriteLine(phoneNumber);
+
+
+static Person GetPerson()
+{
+    Person person = new Person() { Contacts = new Contacts() { PhoneNumber = "123456789" } };
+    return person;
+}
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string MidName { get; set; }
+    public Contacts Contacts { get; set; }
+
+    public string GetFullName()
+    {
+        string fullName = $"{FirstName} {LastName} {MidName}";
+        return fullName;
+    }
+
+    public string GetPhoneNumber()
+    {
+        string phoneNumber = Contacts.PhoneNumber;
+        return phoneNumber;
+    }
+}
+
+public class Contacts
+{
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+}
